b91dcbbb8feece0c66db6b936f1892eb0e742748
guwei0kanyun <guwei@kanyun.com>
2025-07-16 14:47:01 +0000

    Deploying to gh-pages from @ guwei0kanyun/nav@ca2df97776e998c75b1589fd8efffaf67774f475 🚀

--- chunk-C6KLUS3G.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-P6MU33J5.js";import{Ib as e,xb as r}from"./chunk-DHLI5ALU.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
